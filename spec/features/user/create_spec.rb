acbbe77af426757dc6a2a5c0262b601773ed7622
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-06 15:39:58 -0400

    Test: User registration & authentication

--- spec/features/user/create_spec.rb
+++ spec/features/user/create_spec.rb
@@ -6,34 +6,84 @@ RSpec.describe 'Creating a new user:', type: :feature do
     visit '/register'
     expect(page).to have_content("Name")
     expect(page).to have_content("Email")
-    expect(page).to have_button("Save")
+    expect(page).to have_content("Password")
+    expect(page).to have_content("Password Confirmation")
+    expect(page).to have_button("Create User")
   end
 
   it 'happy path' do
     visit '/register'
-    fill_in("Name", with: "Bliffert Blankship")
-    fill_in("Email", with: "Bliff@aol.com")
-    click_button("Save")
+
+    fill_in("Name", with: "Bliffert's Blankship")
+    fill_in("Email", with: "bliffman@gmail.com")
+    fill_in("Password", with: "test")
+    fill_in("Password Confirmation", with: "test")
+    click_button("Create User")
 
     expect(current_path).to eq("/users/#{User.last.id}")
-    expect("#{User.last.name}").to eq("Bliffert Blankship")
+    expect("#{User.last.name}").to eq("Bliffert's Blankship")
   end
 
-  it ' sad- does not create user if Name empty' do
+  it ' sad- does not create user if Name empty and displays error' do
     visit '/register'
 
-    fill_in("Email", with: "Bliff@aol.com")
-    click_button("Save")
+    fill_in("Name", with: "")
+    fill_in("Email", with: "bliffman@gmail.com")
+    fill_in("Password", with: "test")
+    fill_in("Password Confirmation", with: "test")
+    click_button("Create User")
+
     expect(current_path).to eq('/register')
     expect(page).to have_content("Error: Name can't be blank")
   end
 
-  it ' sad- does not create user if email empty' do
+  it ' sad- does not create user if email empty and displays error' do
     visit '/register'
 
     fill_in("Name", with: "Bliffert's Blankship")
-    click_button("Save")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "test")
+    fill_in("Password Confirmation", with: "test")
+    click_button("Create User")
+
     expect(current_path).to eq('/register')
     expect(page).to have_content("Error: Email can't be blank")
   end
+
+  it "can authenticate user registration" do  
+    visit '/register'
+
+     fill_in("Name", with: "Bliffert's Blankship")
+     fill_in("Email", with: "bliffman@gmail.com")
+     fill_in("Password", with: "test")
+     fill_in("Password Confirmation", with: "test")
+     click_button("Create User")
+
+    expect(current_path).to eq("/users/#{User.last.id}")
+  end 
+
+  it "displays an error if password and password confirmation are not matching" do  
+    visit '/register'
+
+     fill_in("Name", with: "Bliffert's Blankship")
+     fill_in("Email", with: "bliffman@gmail.com")
+     fill_in("Password", with: "test")
+     fill_in("Password Confirmation", with: "test1")
+     click_button("Create User")
+
+    expect(current_path).to eq("/register")
+  end 
+
+  it "displays an error if email is already in the database" do  
+    visit '/register'
+    User.create(name: "Bliffert's Blankship", email: "bliffman@gmail.com", password: "test")
+     
+     fill_in("Name", with: "Bliffert's Blankship")
+     fill_in("Email", with: "bliffman@gmail.com")
+     fill_in("Password", with: "test")
+     fill_in("Password Confirmation", with: "test1")
+     click_button("Create User")
+
+    expect(current_path).to eq("/register")
+  end 
 end
