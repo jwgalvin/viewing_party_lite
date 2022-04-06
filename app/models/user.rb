1349dbdedf19334ae5ec88ed4a4ffeed826512df
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-06 15:38:06 -0400

    Test/Feat: User model testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-
+  validates_uniqueness_of :email
+  has_secure_password
+  
   has_many :user_parties
   has_many :parties, through: :user_parties
 
   validates_presence_of :name
   validates_presence_of :email
+  validates_presence_of :password_digest
 
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'attributes' do
+
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password_digest)}
+    it { should have_secure_password}
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
   end
 
   describe 'relationships' do
-      it { should have_many(:user_parties)}
-      it { should have_many(:parties).through(:user_parties)}
+    it { should have_many(:user_parties)}
+    it { should have_many(:parties).through(:user_parties)}
   end
 
-  describe 'methods below here' do
+  it "does not send password through without encryption" do
+    user = User.create(name: 'Tom', email: 'tom@test.com', password: 'password123', password_confirmation: 'password123')
 
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq('password123')
+  end 
+
+  describe 'methods below here' do
+    
   end
 end
