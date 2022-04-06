6d790f72d50bee38ff17b99a203d6d0a9949613d
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-06 15:37:31 -0400

    Add: Password digest column to user

---
+++ db/migrate/20220406182241_remove_password_digestion_from_user.rb
@@ -0,0 +1,6 @@
+class RemovePasswordDigestionFromUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :password_digestion, :string
+    add_column :users, :password_digest, :string
+  end
+end
